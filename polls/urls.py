fdf89c4e9e08680e573bf100ec4c2cb48060ff8b
Tian <tianqi.nan.ru@gmail.com>
2018-03-13 01:40:38 -0400

    First page for project with css and js

--- polls/urls.py
+++ polls/urls.py
@@ -5,13 +5,13 @@ from django.urls import path
 
 from . import views
 
-#Set app_name so that different apps' urls can be used in html
+# Set app_name so that different apps' urls can be used in html
 app_name = 'polls'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    # path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    # path('', views.index, name='index'),
+    path('', views.index, name='index'),
     # path('<int:question_id>/', views.detail, name='detail'),
     # path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
