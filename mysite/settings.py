bc20381eadb63ece37a7b30e7d211bd301c98d67
Tian <tianqi.nan.ru@gmail.com>
2018-03-14 02:04:27 -0400

    AWS Elastic Beanstalk Settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,10 +79,10 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'djangoTest',
-        'USER': 'tian',
+        'NAME': 'Test',
+        'USER': 'tianqinan',
         'PASSWORD': 'riverroad2017',
-        'HOST': '127.0.0.1',
+        'HOST': 'cs539-tian.cptweiginnye.us-east-1.rds.amazonaws.com',
         'PORT': '3306',
     }
 }
